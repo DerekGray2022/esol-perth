4f6d9b2a6609bf26b88df37e3c9e6efc3432e2ce
DerekGray2022 <derekgray99@yahoo.com>
2022-07-10 16:10:46 +0100

    Adjusted Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,34 +1,31 @@
-import { Col,  Navbar } from "react-bootstrap";
+import { Col, Container, Navbar } from "react-bootstrap";
 
 const Footer = () => {
     return (
-        <>
-
-            <Navbar
-                className="footer"
-                bg="esolOrange"
-                variant="dark"
-                sticky="bottom"
-            >
-                <Col>
-                    <Navbar.Brand className="brand">
-                        <img src="assets/images/onBlue_logo.png" width="80px" alt="Logo" />
-                        esolperth
-                    </Navbar.Brand>
 
 
-                </Col>
+        <Navbar
+            className="footer"
+            bg="esolOrange"
+            variant="dark"
+            sticky="bottom"
+        >
+            <Col>
+                <Navbar.Brand className="brand">
+                    <img src="assets/images/onBlue_logo.png" width="80px" alt="Logo" />
+                </Navbar.Brand>
+            </Col>
 
-                <Col className="justify-content-end foot-text">
-                    <Navbar>
-                        Suite 1, Drummond House, 6 Scott Street, Perth, PH1 5EJ UK<br />
-                        +44(0)742 768 9797<br />
-                        esolperth@gmail.com
-                    </Navbar>
-                </Col>
-            </Navbar>
+            <Col className="justify-content-end foot-text">
+                <Navbar>
+                    Suite 1, Drummond House, 6 Scott Street, Perth, PH1 5EJ UK<br />
+                    +44(0)742 768 9797<br />
+                    esolperth@gmail.com
+                </Navbar>
+            </Col>
+        </Navbar>
 
-        </>
+
     );
 };
 
