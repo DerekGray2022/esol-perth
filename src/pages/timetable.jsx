5ebfbd284366295fe7edadda28f37629b9bc1884
DerekGray2022 <derekgray99@yahoo.com>
2022-08-21 19:09:10 +0100

    initial mobile-friendly timetable

--- src/pages/timetable.jsx
+++ src/pages/timetable.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import '../scss/timetable.scss';
 
@@ -9,22 +9,46 @@ const Timetable = () => {
     return (
         <div className="page">
             <Container fluid>
-                
+
                 <h1>September - December 2022</h1>
 
-                <Card className="timetableContainer">
-                    
-                    <Row className="head">
-                        <Col>&nbsp;</Col>
-                        <Col>10:00 - 11:00</Col>
-                        <Col>11: 00 - 12:00</Col>
-                        <Col>12:00 - 13:00</Col>
-                        <Col>13:00 - 14:30</Col>
-                        <Col>15:30 - 17:00</Col>
-                        <Col>18:00</Col>
+                {/*         WEEKDAY BUTTONS      */}
+                <div className="timetableHead">
+                    <Button>MON</Button>
+                    <Button>TUE</Button>
+                    <Button>WED</Button>
+                    <Button>THU</Button>
+                    <Button>FRI</Button>
+                    <Button>SAT</Button>
+                    <Button>SUN</Button>
+                </div>
+                
+                {/*         CONTENT      */}
+                <Card className='timetableContainer'>
+                    <Row>
+                        {/*         Time of Day      */}
+                        <Col className='time' xs={6} sm={5} md={4} lg={3}>
+                            <Row>10:00 - 11:00</Row>
+                            <Row>11:00 - 12:00</Row>
+                            <Row>12:00 - 13:00</Row>
+                            <Row>13:00 - 14:30</Row>
+                            <Row>15:30 - 17:00</Row>
+                            <Row>18:00</Row>
+                        </Col>
+
+                        {/*         Activities       */}
+                        <Col className='activity' xs={6} sm={7} md={8} lg={9}>
+                            <Row>Beginners</Row>
+                            <Row>Intermediate</Row>
+                            <Row className='lunch'>LUNCH</Row>
+                            <Row>Womens' Health</Row>
+                            <Row>Childrens' English Club</Row>
+                            <Row>Beginners | Intermediate</Row>
+                        </Col>
                     </Row>
-                    
-                    <Row className="body">
+                </Card>
+
+                {/* <Row className="body">
                         <Col className='title'>MON</Col>
                         <Col>Beginners</Col>
                         <Col>Intermediate</Col>
@@ -92,9 +116,8 @@ const Timetable = () => {
                         <Col className="shade">----------</Col>
                         <Col className='classes shade'>NO CLASSES</Col>
                         <Col className="shade">----------</Col>
-                    </Row>
+                    </Row> */}
 
-                </Card>
             </Container>
         </div>
     );
