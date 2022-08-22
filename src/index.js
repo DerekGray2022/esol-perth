c162ad712f6b6ea05f65bcb8127dba06b1dad4b7
DerekGray2022 <derekgray99@yahoo.com>
2022-08-22 18:18:27 +0100

    Scroll to top of page on transition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+
 import App from './App';
 import './scss/index.scss'
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>
 );
