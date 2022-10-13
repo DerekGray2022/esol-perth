15ed3d0d1141c91f128529c22f0086b5776a079f
DerekGray2022 <derekgray99@yahoo.com>
2022-10-13 14:54:24 +0100

    Updated Tutors Page

--- src/pages/Tutors.jsx
+++ src/pages/Tutors.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-// import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
-import Spacer from '../components/Spacer';
 
 import '../scss/Timetable.scss';
 import '../scss/Tutors.scss';
@@ -10,17 +9,10 @@ import '../scss/Tutors.scss';
 const Tutors = () => {
     return (
         <>
-            {/*         INTRODUCTION         */}
-            {/* <div className='introTitle'> */}
-                {/*         TITLE        */}
-                <h1>ESOL PERTH Tutors</h1>
-                <Spacer />
-            {/* </div> */}
-
-            {/* <div> */}
+            <div className='introContainer'>
                 {/*         HEADER       */}
-                {/* <Container className='introHeader'>
-                    <Row>
+                <Container className='introHeader'>
+                    {/* <Row> */}
                         <Col className='introCol'>
                             We know that learning is easier when you have an excellent teacher. That's why our tutors keep their qualifications up to date with continuous professional development every year.
                         </Col>
@@ -34,87 +26,109 @@ const Tutors = () => {
                                 </Card.Body>
                             </Card>
                         </Col>
-                    </Row>
-                </Container> */}
+                    {/* </Row> */}
+                </Container>
+
+            </div>
+
+            {/*         TUTOR LIST       */}
+            {/* Senior Tutor */}
+            <Container className="seniorContainer">
+
+                <div className="senior">
+
+                    <div className="seniorText">
+                        <h3>Senior English Language Tutor</h3>
+                        <p>Linda El-Miligy</p>
+                    </div>
+
+                    <img
+                        src="assets/Tutors/Linda-with-student.jpg"
+                        alt="Linda with Student"
+                    />
+
+                </div>
+
+            </Container>
 
 
+            {/* Class Tutors */}
+            <div className="tutorContainer">
 
-                {/*         TUTOR LIST       */}
-                    {/* <Card className='timetableContainer'>
+                <div className="tutorImage">
+                    <img
+                        src="assets/Tutors/Whiteboard.jpg"
+                        alt="Tutor"
+                    />
+                </div>
 
-                        <Card.Body className='timetableCardBody'> */}
+                <div className="tutorList">
+                    <h4>English Language Tutors</h4>
+                    <p>Liz Auld</p>
+                    <p>Alice Carbone</p>
+                    <p>Michael Ellacott</p>
+                    <p>Karen Guthrie</p>
+                    <p>Sarah Jamieson</p>
+                    <p>Helen Kirk</p>
+                    <p>Kasia McGregor</p>
+                    <p>Louise Milne</p>
+                    <p>Joshua Olewole</p>
+                    <p>Agi Parzniewska</p>
+                    <p>Catherine Perrin</p>
+                    <p>Shona Robertson</p>
+                    <p>Lily Scott</p>
+                    <p>Fiona Smith</p>
+                </div>
 
-                            {/*         Senior Tutor       */}
-                            {/* <Card.Title className='timetableCardTitle'>
-                                    <h2>Senior English Language Tutor</h2><p>Linda El-Miligy</p>
-                            </Card.Title> */}
+            </div>
 
-                            {/*     Tutors */}
-                            {/* <Card.Text>
-                                <Row> */}
+            {/* Support Staff */}
+            <div className="supportContainer">
+                <h3>Support Workers</h3>
+                <Row className="supportRow">
 
-                                    {/*         English Language Tutors       */}
-                                    {/* <Col>
-
-                                        <Card className='tutorBox'>
-                                            <h4>English Language Tutors</h4>
-                                            <p>Liz Auld</p>
-                                            <p>Alice Carbone</p>
-                                            <p>Michael Ellacott</p>
-                                            <p>Karen Guthrie</p>
-                                            <p>Sarah Jamieson</p>
-                                            <p>Helen Kirk</p>
-                                            <p>Kasia McGregor</p>
-                                            <p>Louise Milne</p>
-                                            <p>Joshua Olewole</p>
-                                            <p>Agi Parzniewska</p>
-                                            <p>Catherine Perrin</p>
-                                            <p>Shona Robertson</p>
-                                            <p>Lily Scott</p>
-                                            <p>Fiona Smith</p>
-                                        </Card>
-
-                                    </Col> */}
-
-                                    {/*         Support Workers      */}
-                                    {/* <Col> */}
-
-                                        {/*         Voice Coaches       */}
-                                        {/* <Card className='tutorBox'>
-                                            <div>
-                                                <h4>Voice Coaches</h4>
-                                                <p>Edna Auld</p>
-                                                <p>Heather Knudtsen</p>
-                                            </div>
-                                        </Card> */}
-
-                                        {/*         Playworker       */}
-                                        {/* <Card className='tutorBox'>
-                                            <div>
-                                                <h4>Playworker</h4>
-                                                <p>Elsa Tesfamichael</p>
-                                            </div>
-                                        </Card> */}
-
-                                        {/*         Administrator        */}
-                                        {/* <Card className='tutorBox'>
-                                            <div>
-                                                <h4>Administrator</h4>
-                                                <p>Jess Jack</p>
-                                            </div>
-                                        </Card>
-
-                                    </Col>
-
-                                </Row>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img
+                            variant="top"
+                            src="assets/Tutors/Support/VoiceCoaches.jpg"
+                        />
+                        <Card.Body>
+                            <Card.Title><b>Voice Coaches</b></Card.Title>
+                            <Card.Text>
+                                <p>Edna Auld</p>
+                                <p>Heather Knudtsen</p>
                             </Card.Text>
-
                         </Card.Body>
+                    </Card>
 
-                    </Card> */}
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img 
+                        variant="top"
+                            src="assets/Tutors/Support/Playworker.jpg"
+                         />
+                        <Card.Body>
+                            <Card.Title><b>Playworker</b></Card.Title>
+                            <Card.Text>
+                                <p>Elsa Tesfamichael</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
 
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img 
+                        variant="top"
+                            src="assets/Tutors/Support/Administrator.jpg"
+                         />
+                        <Card.Body>
+                            <Card.Title><b>Administrator</b></Card.Title>
+                            <Card.Text>
+                                <p>Jess Jack</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
 
-            {/* </div> */}
+                </Row>
+            </div>
         </>
     );
 };
